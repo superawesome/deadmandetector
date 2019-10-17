669f9eea1596757bbaf8b35e9477329364f850a8
Norman Maul <nmaul@cloudera.com>
2019-10-16 20:45:23 -0700

    cleaning up - removing /list and associated bits, switching to one gauge with many labels

--- app.py
+++ app.py
@@ -1,16 +1,13 @@
-from flask import Flask, request, render_template
-from datetime import datetime, timedelta
+from flask import Flask, request
 from werkzeug.wsgi import DispatcherMiddleware
 from prometheus_client import make_wsgi_app, Gauge
-import time
-
 
 app = Flask(__name__)
 
 # adds the prometheus /metrics route
 app_dispatch = DispatcherMiddleware(app, {'/metrics': make_wsgi_app()})
 
-gauges = {}
+g = Gauge('customer_prometheus_watchdog_seconds', "Prometheus Deadman timestamp", ['customer', 'environment'], multiprocess_mode='max')
 
 @app.route('/')
 def index():
@@ -22,18 +19,5 @@ def ping():
     customer = req_data['commonLabels']['customer']
     environment = req_data['commonLabels']['environment']
     key = "{}:{}".format(customer, environment)
-    try: # update existing Gauge
-        gauges[key].set_to_current_time()
-    except KeyError: # Gauge doesn't exist yet, create then update it
-        gauges[key] = Gauge(key, 'Prometheus Deadman timestamp', multiprocess_mode='max')
-        gauges[key].set_to_current_time()
-    return '{} - {}\n'.format(key, gauges[key]._value.get())
-
-@app.route('/list')
-def list():
-    curtime = datetime.now()
-    return render_template('list.html', curtime=curtime, status=gauges)
-
-@app.template_filter('dt')
-def _jinja2_filter_datetime(t):
-    return time.ctime(t)
+    g.labels(customer, environment).set_to_current_time()
+    return '{} - {}\n'.format(key, g.labels(customer, environment)._value.get())
